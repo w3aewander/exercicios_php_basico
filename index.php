9499fd6b3cc0a4e699991e9c1f4f3ccf860d16c1
w3aewander <wander.silva@gmail.com>
2023-05-14 02:04:06 -0300

    Primeiro commit

---
+++ index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Resolução de Exercícios</title>
+  <link rel="stylesheet" href="css/app.css">
+
+</head>
+<body>
+  
+  <?php include "./exercicio.php"; ?>
+  
+  <div class="header">
+    <h1>Resolução de exercícios</h1>
+    <p>
+      Wanderlei Silva do Carmo - Exercícios orientados <br>
+      <h2 class="text-center">Resposta dos exercícios</h2>
+      <p> Clique <a href="./docs/exercicios01.pdf" target="__blank"><strong>aqui</strong></a> para visualizar a folha de exercícios</p>
+    </p>
+  </div>
+  <div class="container box-shadow">
+    <?php include "ex01.php"; ?>
+  </div>
+
+  <div class="container box-shadow">
+    <?php include "ex02.php"; ?>
+  </div>
+
+  <div class="container box-shadow">
+    <?php include "ex03.php"; ?>
+  </div>
+
+  <div class="container box-shadow">
+    <?php include "ex04.php"; ?>
+  </div>
+
+  <div class="container box-shadow">
+    <?php include "ex05.php"; ?>
+  </div>
+  
+  <div class="container box-shadow">
+    <?php include "ex06.php"; ?>
+  </div>
+
+</body>
+
+
+</html>
+   
