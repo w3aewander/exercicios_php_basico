5f1075d5e5c38556d33929392d6bd27cd8834f22
w3aewander <wander.silva@gmail.com>
2023-05-15 21:30:42 -0300

    Conteúdo das aulas teóricas

--- index.php
+++ index.php
@@ -1,11 +1,12 @@
-<?php 
- date_default_timezone_set("America/Sao_Paulo");
- include __DIR__ . "/header.php"; 
+<?php
+date_default_timezone_set("America/Sao_Paulo");
+include __DIR__ . "/header.php";
 ?>
 <div class="header">
   <h1>Curso Fundamentos de PHP</h1>
-  <p>Prof. Wanderlei Silva do Carmo <wander.silva@gmail.com></p>
-  <?=date('d/m/Y H:i:s');?>
+  <p>Prof. Wanderlei Silva do Carmo <wander.silva@gmail.com>
+  </p>
+  <?= date('d/m/Y H:i:s'); ?>
 </div>
 
 <div class="menu-header">
@@ -21,43 +22,40 @@
   </p>
 </div>
 
-  <!-- constantes -->
-  <div id="constante" class="container box-shadow">
-    <?php require __DIR__ . "/constantes.php" ; ?>
-    <a href="javascript:scrollTo(0,0);">Voltar ao topo</a>
-  </div
+<!-- constantes -->
+<div id="constante" class="container box-shadow">
+  <?php require __DIR__ . "/constantes.php"; ?>
+  <a href="javascript:scrollTo(0,0);">Voltar ao topo</a>
+</div> 
 
-  <!-- variaveis -->
-  <div id="variaveis" class="container box-shadow">
-    <?php require __DIR__ . "/variaveis.php" ; ?>
-    <a href="javascript:scrollTo(0,0);">Voltar ao topo</a>
-  </div
-
-  <!-- vetores -->
-  <div id="vetores" class="container box-shadow">
-    <?php require __DIR__ . "/vetores.php" ; ?>
-    <a href="javascript:scrollTo(0,0);">Voltar ao topo</a>
-  </div
-
-  <!-- funções -->
-  <div id="funcs" class="container box-shadow">
-    <?php require __DIR__ . "/funcs.php" ; ?>
-    <a href="javascript:scrollTo(0,0);">Voltar ao topo</a>
-  </div
-
-  <!-- manipulação de arquivos -->
-  <div id="arquivos" class="container box-shadow">
-    <?php require __DIR__ . "/arquivos.php" ; ?>
-    <a href="javascript:scrollTo(0,0);">Voltar ao topo</a>
+<!-- variaveis -->
+<div id="variaveis" class="container box-shadow">
+  <?php require __DIR__ . "/variaveis.php"; ?>
+  <a href="javascript:scrollTo(0,0);">Voltar ao topo</a>
+</div> <!-- vetores -->
+<div id="vetores" class="container box-shadow">
+  <?php require __DIR__ . "/vetores.php"; ?>
+  <a href="javascript:scrollTo(0,0);">Voltar ao topo</a>
 </div>
-  <!-- video aulas -->
-  <div id="videos" class="container box-shadow">
-    <?php require __DIR__ . "/videos.php" ; ?>
-    <br>
-    <div class="container box-shadow">
-      <a href="javascript:scrollTo(0,0);">Voltar ao topo</a>
-    </div>
-  </div
-  
-  
+
+<!-- funções -->
+<div id="funcs" class="container box-shadow">
+  <?php require __DIR__ . "/funcs.php"; ?>
+  <a href="javascript:scrollTo(0,0);">Voltar ao topo</a>
+</div> 
+
+<!-- manipulação de arquivos -->
+<div id="arquivos" class="container box-shadow">
+  <?php require __DIR__ . "/arquivos.php"; ?>
+  <a href="javascript:scrollTo(0,0);">Voltar ao topo</a>
+</div>
+<!-- video aulas -->
+<div id="videos" class="container box-shadow">
+  <?php require __DIR__ . "/videos.php"; ?>
+  <br>
+  <div class="container box-shadow">
+    <a href="javascript:scrollTo(0,0);">Voltar ao topo</a>
+  </div>
+</div>
+
 <?php include __DIR__ . "/footer.php"; ?>
